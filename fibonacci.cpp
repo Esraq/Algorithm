9caf7120d0590cd5ca29be7967b806162380cc55
esraq <esraq26552@gmail.com>
2025-12-04 11:43:42 +0600

    all code

---
+++ fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    int dp[n+1];
+
+    // Base cases
+    dp[0] = 0;
+    dp[1] = 1;
+
+    // Fill the DP table
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Fibonacci(" << n << ") = " << fibonacci(n) << endl;
+
+    return 0;
+}
